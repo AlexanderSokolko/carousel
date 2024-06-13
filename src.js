1ab4ef0ecd7eabf3dcfca85105bc5cad89feed91
AleksanderSokolko <alex_sokolko1999@mail.ru>
2024-06-13 18:04:06 +0400

    first commit

---
+++ src.js
@@ -0,0 +1,14 @@
+function handleImageChange(offset) {
+  const activeSlide = document.querySelector("[data-active]");
+  const slides = [...document.querySelectorAll(".carousel-image")];
+  const currentIndex = slides.indexOf(activeSlide);
+  let newIndex = currentIndex + offset;
+
+  if (newIndex < 0) newIndex = slides.length - 1;
+  if (newIndex >= slides.length) newIndex = 0;
+  slides[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
+
+const onNext = () => handleImageChange(1);
+const onPrev = () => handleImageChange(-1);
